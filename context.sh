130b8855662a32a036ea5608444c3d1d8f46f042
app2linux <46576676+app2linux@users.noreply.github.com>
2019-01-18 20:09:52 +0100

    Update context.sh

--- context.sh
+++ context.sh
@@ -2,33 +2,39 @@
 # This script has been tested on Debian 8 Jessie image
 #
 # chmod +x ./context.sh
-#
+#  ---------------------------------------------------------
 if [ "$EUID" -ne 0 ]; then echo "Must be root"; exit; fi
 echo -e "Environamental variables."
 ifconfig eth0 | grep inet | awk '{ print $2 }'
 #
 export RELEASE=$(lsb_release -cs)
 #
-export mainDomain="mydomain.com"
-export mainIP="10.0.0.21"
-export mainColor='#898E8C'
+export mainDomain=""
+export mainIP=""
+export mainColor="#BA0020"
 #
 export auxDomain=''
 export auxIP='' 
 #
-export errorDir=/var/www/error
 export appFolder="/app/nginx"
 export wwwFolder="/var/www"
+export errorDir=/var/www/error
+export errorStyle=True 		
+#errorStyle=True for Global, [/var/www/error]  | =False for Local, [www<site>/style dir]
+color=(#DC443A #98243A #11589F #715138 #D69C2F #616247 #898E8 #2e5090C #5F4B8B #BA0020 #0E3A53)
 #
 echo "# Data context web : [Config folder: " ${appFolder} "]  [Web folder: "${wwwFolder}"]"
 echo "# --------------------------------------------------------------------------"
 echo "# Data order [ domain : IP : workDir nameSite colorSite subDirIn ]"
 echo "# --------------------------------------------------------------------------"
 export context=("(domain IP workDir nameSite colorSite subDirIn)"
-"("${mainDomain}" "${mainIP}" html mainSite #2e5090)"
-"(blog."${mainDomain}" "${mainIP}" blog blogSite #5F4B8B mainSite)"
-"(cloud."${mainDomain}" "${mainIP}" cloud cloudSite #BA0020 mainSite)"
-"(iot."${mainDomain}" "${mainIP}" iot iotSite #0E3A53 mainSite)"
+"("${mainDomain}" "${mainIP}" html mainSite "${mainColor}")"
+"()"
+"()"
+"()"
+"()"
+"()"
+"()"
 "()"
 "()"
 ) 
@@ -44,4 +50,6 @@ do
     done
 echo
 done
+export verifiedContext=false
 echo "# --------------------------------------------------------------------------"
+
