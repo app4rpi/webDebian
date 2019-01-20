881e37d51c932886cdfce18e7eefa5e7f1eb28ca
app2linux <46576676+app2linux@users.noreply.github.com>
2019-01-20 22:24:58 +0100

    Update context.sh

--- context.sh
+++ context.sh
@@ -20,11 +20,8 @@ export appFolder="/app/nginx"
 export wwwFolder="/var/www"
 export errorDir="/var/www/error"
 export errorStyleLocal=false 		
-#errorStyleLocal=False for Global, [/var/www/error]  | =True for Local, [www<site>/style dir]
 color=('#DC443A' '#98243A' '#11589F' '#715138' '#D69C2F9' '#616247' '#898E88' '#2e5090C' '#5F4B8B' '#BA0020' '#0E3A53' #DC443A' '#98243A' '#11589F' '#715138' '#D69C2F9' '#616247' '#898E88' '#2e5090C' '#5F4B8B')
 #
-echo "# Data context web : [Config folder: " ${appFolder} "]  [Web folder: "${wwwFolder}"]"
-echo "# Error Style Local : "${errorStyleLocal}
 echo "# --------------------------------------------------------------------------"
 echo "# Data order [ domain : IP : workDir nameSite colorSite subDirIn ]"
 echo "# --------------------------------------------------------------------------"
