097872514bcdcd365efbf7260d95a14fa2f9c493
app2linux <46576676+app2linux@users.noreply.github.com>
2019-01-21 21:00:09 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,54 +1,72 @@
-!/bin/bash
+#!/bin/bash
 # This script has been tested on Debian 8 Jessie image
 #
 # chmod +x ./setup.sh
 #  ---------------------------------------------------------
 if [ "$EUID" -ne 0 ]; then echo "Must be root"; exit; fi
+#  ---------------------------------------------------------
 LINE="---------------------------------------"
 lineOrder="${@,,} "
 #  ---------------------------------------------------------
 clear 
 echo -e "#\nAutomatic install & config & start server and web server."
 echo -e '# '$LINE$LINE
-echo -e  'Line orders usage & options (all in the same line):'
-echo -e   './setup.sh <nameMainDomain>.<extension> <subdomain> <subdomain> <...> \'
-echo -e " [NOIP] [ERRORLOCAL] [dav=<davServer>] [user=<davUser>] [pw=davPw]"
-echo -e '# '$LINE$LINE
-#  ---------------------------------------------------------
-echo 'LO : '${lineOrder}
-#
-echo
-echo 'WebDav backup config:'
-echo $LINE
-[[ ${lineOrder} = *"dav="* ]] && { dav=${lineOrder##*dav=};dav=dav=${dav%% *}; echo 'DAV: '$dav; lineOrder=${lineOrder//$dav /}; } || { dav=""; echo 'WebDav -> not present'; }
-[[ ${lineOrder} = *"user="* ]] && { user=${lineOrder##*user=};user=user=${user%% *}; echo 'USR: '$user; lineOrder=${lineOrder//$user /}; } || { user=""; echo 'User -> not present'; }
-[[ ${lineOrder} = *"pw="* ]] &&  { pw=${lineOrder##*pw=}; pw='pw='${pw%% *};  echo 'PW : '$pw;  lineOrder=${lineOrder//$pw /}; } || { pw=""; echo 'Password -> not present'; }
-echo
-echo 'Web server parameters:'
-echo $LINE
-[[ ${lineOrder} = *"noip"* ]] && { noip='NOIP'; echo 'NOIP -> active'; lineOrder=${lineOrder//'noip'/}; } || { noip=''; echo 'NOIP -> not present'; }
-[[ ${lineOrder} = *"errorlocal"* ]] && { errorlocal='ERRORLOCAL'; echo 'ERRORLOCAL -> active'; lineOrder=${lineOrder//'errorlocal'/}; } || { errorlocal=''; echo 'ERRORLOCAL -> not present'; }
-if [[ ${lineOrder[@]} = *"."* ]]; then
-exten=${lineOrder##*.} ;domain=${lineOrder//.$exten/};
-exten=${exten%% *}; domain=${domain##* };
-subdomains=${lineOrder//$domain.$exten/};
-[[ ! $domain || ! $exten ]] && { domain='';echo 'DOMAIN -> not present'; } || { domain=$domain.$exten; echo 'DOMAIN: '$domain; }
-fi
-#  ---------------------------------------------------------
-echo 'SUBDOMAINS: '${subdomains}
-echo $LINE$LINE
-while true; do  read -r -p "Are all correct & continue? [Y/n] " input
-  case $input in 
-	[yY][eE][sS]|[sS][iI]|[yYsS]) break ;;
-	[nN][oO]|[nN]) exit 0 ;; 
-  	*) echo -n  "Invalid input...  ->   " ;;
- esac
-done
 #  ---------------------------------------------------------
 # Initial issues
 #  ---------------------------------------------------------
 # Config system
+# setup server
+#./setupServer.sh
 #  ---------------------------------------------------------
-# final issues
+# startup
+echo "Defineix paràmetres del context de la web amb NGIX:"
+echo " · Ús o no de la IP pública del servidor, "
+echo " · Ús o no del nom de domini y subdominis"
+echo " · Un únic arxiu d'estils d'errors o múltiples arxius per a cada domini y subdomini."
+echo " · Creació de l'estructura de directoris i arxius a partir de l'arrel '/var/www'"
+echo " · Creació dels arxius de configuració de la web per a NGINX al directori '/app/nginx'"
+echo " · Configurar còpies de seguretat a un WebDav server"
+echo "També es podrà editar l'arxiu de configuració [nano context.sh] i, a més, afegir dominis, IP,..."
+echo $LINE$LINE
+echo "Web config options in line orders:" 
+echo "(all in the same line):"
+echo "----------------------------------"
+echo "    <nameMainDomain>.<extension> <subdomain> <subdomain> <...> \ "
+echo "        [NOIP] [ERRORLOCAL] \ "
+echo
+echo "    [x]  Cancel & Return"
+echo -e $LINE$LINE 
+read -p "   > " lineOrder
+#[[ $lineOrder -ne "x" ]] && echo be || echo surt
 
+#./startup.sh $lineOrder
+#  ---------------------------------------------------------
+echo
+echo -e $LINE$LINE 
+echo "Defineix SSL:"
+#  ---------------------------------------------------------
+echo
+echo -e $LINE$LINE 
+echo "Defineix copia de seguratat:"
+echo "(tots obligatoris):"
+echo "----------------------------------"
+echo "    [dav=<davServer>] [user=<davUser>] [pw=davPw]"
+echo
+echo "    [x]  Cancel & Return"
+echo -e $LINE$LINE 
+read -p "   > " lineOrder
+#[[ $lineOrder -ne "x" ]] && echo be || echo surt
+#  ---------------------------------------------------------
+# Create files & folders for www and nginx
+# nginxConfig
+#  ---------------------------------------------------------
+# final issue
+sed -i '8,$d' setup.sh
+#sed -ie "s/^# chmod +x.*$/cd startup; start.sh; cd ..;/" setup.sh
+cat <<'EOF' >> setup.sh
+cd startup
+start.sh
+cd ..
+exit
+EOF
 exit
