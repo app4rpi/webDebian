d72e3a0f3cac3c87493c6be5395f27c3fefaf8d2
app2linux <46576676+app2linux@users.noreply.github.com>
2019-01-25 16:40:53 +0100

    Create letsencrypt.sh

---
+++ letsencrypt.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This script has been tested on Debian 8 Jessie image
+# chmod +x ./letsencrypt.sh
+#  ---------------------------------------------------------
+echo " Configure SSL letsencrypt"
+exit
