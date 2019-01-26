beb4b9b4575dc32a9fbcdf8adf1117fdfa46b499
app2linux <46576676+app2linux@users.noreply.github.com>
2019-01-26 16:58:48 +0100

    Update setupDav.sh

--- setupDav.sh
+++ setupDav.sh
@@ -4,12 +4,7 @@
 #  ---------------------------------------------------------
 if [ "$EUID" -ne 0 ]; then echo "Must be root"; exit; fi
 LINE="-------------------------------------------------------"
-lineOrder="${@,,} "
-FILE="/etc/davfs2/secrets"
-POINT=/mnt/dav
-BACKUPDIR=/backup/$mainDomain
-DIR2SYNC=(/app /var/www /etc/letsencrypt)
-[[ ! -d ${POINT} ]] && mkdir ${POINT}
+lineOrder="${@}"
 #  ---------------------------------------------------------
 echo -e "\nBackup configuration parameters"
 echo $LINE$LINE
@@ -20,16 +15,25 @@ echo 'Dav: ' $webDav
 echo 'Usr: ' $user
 [[ ${lineOrder} = *"pw="* ]] && { pw=${lineOrder//*pw=/};pw=${pw// */}; } || pw=''
 echo 'Pw : ' $pw
+[[ ${lineOrder} = *"dir="* ]] && { dir=${lineOrder//*dir=/};dir=${dir// */}; } || dir=''
+echo 'Dir: ' $dir
 echo $LINE$LINE
-
-if [[ ! $webDav || ! $user || ! $pw ]]; then
+[[ ! $dir ]] && dir=$mainDomain
+if [[ ! $webDav || ! $user || ! $pw || ! $dir ]]; then
     echo -e "   >  Incorrect mandatory parameters"
     echo $LINE$LINE
     echo
     read -n 1 -s -r -p "  Press any key to continue > "
+    echo
     exit 0
 fi
+#  ---------------------------------------------------------
+FILE="/etc/davfs2/secrets"
+POINT=/mnt/dav
+BACKUPDIR=/backup/$dir
+DIR2SYNC=(/app /var/www /etc/letsencrypt)
 #
+[[ ! -d ${POINT} ]] && mkdir ${POINT}
 hostDav=${webDav//*:\/\//};hostDav=${hostDav%/*};
 if ! ping -c 1 -W 1 "$hostDav"; then
     echo -e "   >  Server <$hostDav> not available.\n      Select another cloud service for the backup"
@@ -37,17 +41,12 @@ if ! ping -c 1 -W 1 "$hostDav"; then
     echo
     read -n 1 -s -r -p "  Press any key to continue > "
     echo
-    read -n 1 -s -r -p "  Press any key to continue > "
-    echo
     exit 0
 fi
 #
-#dirDav=${webDav//*:\/\//};dirDav=${dirDav%.*};[[ ${dirDav} = *"."* ]] && dirDav=${dirDav##*.}Dav;
-if $(grep -q ^https: /etc/davfs2/secrets); then echo nameserver 1.1.1.1
-    sed -ie "0,/^https:/{s/^https:/#https:/}" /etc/davfs2/secrets
-    sed -ie "0,/^https:/{s/^https:/#https:/}" /etc/fstab
-fi
-echo $webDav $user $pw >> /etc/davfs2/secrets
+sed -ie "0,/^\/mnt\//{s/^\/mnt\//#\/mnt\//}" /etc/davfs2/secrets
+sed -ie "0,/^https:/{s/^https:/#https:/}" /etc/fstab
+echo $POINT $user $pw >> /etc/davfs2/secrets
 echo $webDav $POINT davfs rw,user,uid=root,noauto 0 0 >> /etc/fstab
 [[ ! -d ${POINT} ]] && mkdir ${POINT}
 #
@@ -56,18 +55,19 @@ if [[ ! -f syncDav.sh ]]; then
     temp+="# chmod +x ./syncDav.sh\n#  ---------------------------------------------------------\nDIR2SYNC=("
     temp+=${DIR2SYNC[@]}
     temp+=")\nmount ${POINT}\n"
-    temp+="if ! mount | grep ${POINT} >/dev/null; then\n   echo 'ERROR: No backup devices'\n   exit\nfi\n"
-    temp+="[[ ! -d ${POINT}${BACKUPDIR}/ ]] && mkdir -p ${POINT}${BACKUPDIR}/\n"
+    temp+="if ! mount | grep ${POINT} > /dev/null; then\n   echo 'ERROR: No backup devices'\n   exit\nfi\n"
+    temp+="[[ ! -d ${POINT}${BACKUPDIR} ]] && mkdir -p ${POINT}${BACKUPDIR}\n"
     temp+="echo\necho Start backup ...\necho Remote backup dir: ${webDav}${BACKUPDIR}\n"
     temp+="echo '---------------------------------------------------------'\n"
     temp+="[[ ! -d ${POINT}${BACKUPDIR}/ ]] && { echo 'Unable Backup Dir'; exit; }\n"
     temp+="for DIR in \${DIR2SYNC[*]} ; do\n"
-    temp+="    [[ -d \${DIR} ]] && rsync -rPz \${DIR} ${POINT}${BACKUPDIR}/ || echo \${DIR} ' not exist!'\n"
-    temp+="done\numount ${POINT}\nexit\n"
+    temp+="    [[ -d \${DIR} ]] && rsync -urP -var --progress --delete \${DIR} ${POINT}${BACKUPDIR}/ || echo \${DIR} ' not exist!'\n"
+    temp+="done\numount ${POINT}\n"
+    temp+='    read -n 1 -s -r -p "  Press any key to continue > '
+    temp+="\nexit\n"
     echo -e $temp >> ./syncDav.sh
     chmod +x ./syncDav.sh
     ln syncDav.sh ../backup.sh
 fi
 ./syncDav.sh
 exit
-
