8b8e74d71bb19203ab0931b42066bcf04dc52585
app2linux <46576676+app2linux@users.noreply.github.com>
2019-02-05 20:40:30 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,115 +21,7 @@ return 1
 #  ---------------------------------------------------------
 clear 
 echo -e "#\nAutomatic install & config & start server and web server."
-echo -e '# '$LINE$LINE
-#  ---------------------------------------------------------
-# Initial issues
-file=(start.sh setupServer.sh startup.sh context.sh nginxConfig.sh setupDav.sh letsencrypt.sh nginxStart.sh sslConfig.sh test.sh)
-echo -en 'Create the <./startup> directory for the shell files and copy git files  : '
-[ ! "$(ls -A ./startup)" ] && mkdir -p ./startup 
-for ((i=0; i<${#file[@]}; i++)); do
-    echo -n '<'${file[i]}'> : '
-    [[ -f ./startup/ ]] && continue
-    [[ -f ./startup/${file[i]} ]] && continue
-    wget https://raw.githubusercontent.com/app2linux/webDebian/master/${file[i]} -P ./startup
-    chmod +x ./startup/${file[i]}
-    done
-echo
-[ ! "$(ls -A ./startup)" ] && { rmdir ./startup; exit; }
-cd startup
-#
-#  ---------------------------------------------------------
-# Update System
-RELEASE=$(lsb_release -cs)
-#  ----------------------------------
-cat /etc/issue
-echo "#  ----------------------------------"
-echo 'Update system: '${RELEASE}
-# add nameservers
-file="/etc/resolv.conf"
-echo -e "\n DNS servers\n"$LINE$LINE
-nameServers=("8.8.8.8" "1.1.1.1" "9.9.9.9" "208.67.222.222" "8.8.4.4" "149.112.112.112" "208.67.220.220")
-for name in ${nameServers[*]} ; do
-# echo 'nameserver '$name
-#sed -e "\|$name|h; \${x;s|$name||;{g;t};a\\" -e "$name" -e "}" $file
-[[ ! $(sed -n "/^nameserver $name/p;q" $file) ]] && echo 'nameserver '$name >> $file
-done
-cat $file
-echo $LINE$LINE
-# Update sources list
-file='/etc/apt/sources.list'
-echo -e 'File to update : '$file
-[[ -f $file && ! -f $file.old ]] && mv $file $file.old
-cat <<EOF  > $file
-deb http://ftp.debian.org/debian/ ${RELEASE} main contrib non-free
-deb-src http://ftp.debian.org/debian/ ${RELEASE} main contrib non-free
-deb http://security.debian.org/ ${RELEASE}/updates main contrib
-deb-src http://security.debian.org/ ${RELEASE}/updates main contrib
-deb http://ftp.debian.org/debian/ ${RELEASE}-updates main contrib non-free
-deb-src http://ftp.debian.org/debian/ ${RELEASE}-updates main contrib non-free
-deb http://httpredir.debian.org/debian ${RELEASE} main contrib
-deb-src http://httpredir.debian.org/debian ${RELEASE} main contrib
-EOF
-#
-echo "# -----------"
-#  ---------------------------------------------------------
-# Config system
-# setup server
-./setupServer.sh
-echo -e $LINE"\n\n>  Server already uptated\n"$LINE
-read -rsn1 -p "Press any key to continue > "
-#  ---------------------------------------------------------
-# startup: define context web
-clear
-if $(grep -q verifiedContext=true context.sh); then 
-    echo
-    ./context.sh
-    echo -e "\n<context.sh> already configured. To change, delete the file first ..."
-    echo $LINE$LINE
-    isContinue
-else
-    reLoop=true
-    while $reLoop; do
-        echo -e "\nDefines context settings for the web with NGINX:"
-        echo $LINE$LINE
-        echo " · Use or not the server's public IPr, "
-        echo " · Use or not the domain name and subdomains"
-        echo " · A single file of error styles or multiple files for each domain and subdomain."
-        echo " · Creation of the directory structure and files from the root '/var/www"
-        echo " · Creation of the web configuration files for NGINX in the directory '/app/nginx'"
-        echo " · Configure backup copies with a WebDav server"
-        echo "You can also edit the configuration file [nano context.sh] and, additionally, add domains, IP, ..."
-        echo $LINE$LINE
-        echo -e "\nWeb config options in line orders:" 
-        echo "(all in the same line):"
-        echo $LINE
-        echo "    <nameMainDomain>.<extension> <subdomain> <subdomain> <...> \ "
-        echo "        [NOIP] [ERRORLOCAL] \ "
-        echo
-        echo "    [x]  Cancel & Return"
-        echo -e $LINE$LINE 
-        read -p "   > " lineOrder
-        #./startup.sh $lineOrder
-        #./test.sh $lineOrder
-        if [[ $lineOrder =~ ^(x|X) ]]; then
-            echo
-            exit 
-        else
-            echo -e $LINE$LINE 
-            echo -e "\n\nModification of the configuration file"
-            ./startup.sh $lineOrder
-        fi
-        valor=$(echo "$?")
-        [[ "${valor}" == 1 ]] && break
-        done
-fi
-#
-#  ---------------------------------------------------------
-# Create files & folders for www and nginx
-echo -e "\n\nCreate files & folders for www and nginx"
-echo -e $LINE$LINE 
-if $(grep -q folderCreated=true context.sh); then 
-    echo -e "\n >  Files & folders already configurated."#!/bin/bash
+#!/bin/bash
 # This script has been tested on Debian 8 Jessie image
 # chmod +x ./setup.sh
 # wget https://raw.githubusercontent.com/app2linux/webDebian/master/setup.sh
@@ -350,7 +242,7 @@ else
         [[ "${valor}" == 1 ]] && break
         done
     echo -e "\n"$LINE$LINE
-    echo -en "\t"; read -rsn1 -p "Press any key to continue  > "
+    echo -en "\t"; read -n 1 -s -r -p "Press any key to continue  > "
     fi
 echo
 return
