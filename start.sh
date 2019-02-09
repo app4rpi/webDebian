d714858f555971a8d537efabaf8a9404bbf83477
app2linux <46576676+app2linux@users.noreply.github.com>
2019-02-09 17:27:17 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,19 +4,8 @@
 #  ---------------------------------------------------------
 if [ "$EUID" -ne 0 ]; then echo "Must be root"; exit; fi
 LINE="-------------------------------------------------------"
+source ./functions.sh
 #  ---------------------------------------------------------
-function isCorrect(){
-while true; do  echo -en "\t"
-read -r -p "Are all correct & continue? [Y/n] " input
-  case $input in 
-	[cC][yY][eE][sS]|[sS][iI]|[yYsS]) return 1 ;;
-	[xX][nN][oO]|[nN]) return 0 ;; 
-  	*) echo -en  "\tInvalid input...  ->   " ;;
- esac
-done
-return 1
-}
-# --------------------------------------------------------------------------
 function initialIssues(){
 echo -e 'Initial issues ... '
 return 1
@@ -27,81 +16,23 @@ echo -e '\nFinal issues ... '
 return 1
 }
 # --------------------------------------------------------------------------
-function downloadGit(){
-echo -e '\n'$LINE'\nDownload git files:'
-echo -e "\tCopy & overwriting all bash script files except <context.sh>"
-echo -e "\tTo modify <context.sh> erase file first\n"$LINE
-isCorrect; val=$?;
-[[ $val == 0 ]] && return
-file=(start.sh setupServer.sh startup.sh nginxConfig.sh setupDav.sh letsencrypt.sh nginxStart.sh sslConfig.sh test.sh)
-echo-n "Bash files: "
-for ((i=0; i<${#file[@]}; i++)); do
-    echo -n '<'${file[i]}'> : '
-    wget -q https://raw.githubusercontent.com/app2linux/webDebian/master/${file[i]} -P ./
-    chmod +x ./${file[i]}
-    done
-[[ ! -f ./context.sh ]] && wget -q https://raw.githubusercontent.com/app2linux/webDebian/master/context.sh -P ./
-echo -e "\n"$LINE$LINE"\n\n\tExit now & restart bash script file \n"
-echo -en "\t"; read -rsn1 -p "Press key to continue -> " key
-echo
-exit
-}
-#  ----------------------------------
-function restartContext(){
-echo -e '\n'$LINE'\nDelete <context.sh> config files,'
-echo -e "Restore from file or download new file from GitUb\n"$LINE
-isCorrect; val=$?;
-[[ $val == 0 ]] && return
-mv ./context.sh ./context.old
-[[ -f context.bak ]] && cp ./context.bak ./context.sh || { wget https://raw.githubusercontent.com/app2linux/webDebian/master/context.sh -P ./; chmod +x ./context.sh; }
-echo -e "\tFile <context.sh> restored."
-echo -en "\t"; read -rsn1 -p "Press key to continue -> " key
-return 1
-}
-#  ----------------------------------
-function viewContext(){
-source ./context.sh
-echo
-echo $LINE$LINE
-echo -e "\nEnvironamental variables.\n"$LINE$LINE
-echo -e "Main domain:[$mainDomain]   IP:[$mainIP]\n"$LINE
-echo -en "Folders:  App:[$appFolder]  Backup:[$backupFolder]  www:[$wwwFolder]  Error dir:[$errorDir]  "
-echo -en "Error style: "
-[[ ${errorStyleLocal} = true ]] && echo 'Global' || echo 'Local'
-echo -e "SSL/TLS: [$SSL]   email:[$SSLemail]   Domains:[$SSLdomains]"
-temp=($DAVconfig)
-echo -e "WebDav:  Server:[${temp[0]}]   email:[${temp[2]}]   Pw:[${temp[3]}]"
-echo -e $LINE$LINE"\n[domain IP workDir nameSite colorSite subDirIn]\n"$LINE
-title=(${context[0]:1:-1})
-for ((i=1; i<${#context[@]}; i++));  do
-    data=(${context[i]:1:-1})
-    [[ -z $data ]] && break
-    for ((j=0; j<${#data[@]}; j++)); do
-      echo -n "["${data[j]}']  '
-      done
-    echo
-    done
-echo $LINE$LINE
-echo -en "\t"; read -rsn1 -p "Press key to continue -> " key
-return 1
-}
-
-# --------------------------------------------------------------------------
 function updateWeb() {
 while true; do
     echo -e "\n"$LINE"\n\tUpdate web config\n"$LINE 
     echo -e "  1. View <context.sh> config files"
-    echo -e "  2. Restart <context.sh> config file"
-    echo -e "  3. Add main domain & subdomains"
-    echo -e "  4. Modify domains & subdomains"
-    echo -e "  4. Delete domains & subdomains"
+    echo -e "  r. Restart <context.sh> config file"
+    echo -e "  3. Add domain & config files"
+    echo -e "  4. Delete domains & config files"
+    echo -e "  7. Verify config files & web structure"
     echo -e "  x. Exit\n"$LINE
     echo -en "\t"; read -rsn1 -p "Enter choice -> " key
     case $key in
         1) viewContext ;;
-        2) restartContext ;;
+        r|R) restartContext ;;
+        3) manageDomain add ;;
+        4) manageDomain del ;;
+        7) verifyConfig ;;
         x) break ;;
-        *) echo -e "${RED} Error...${STD}" && sleep 2 ;;
         esac
     done
 return
@@ -120,7 +51,24 @@ while true; do
         1) downloadGit ;;
         2) ./setupServer.sh ;;
         x) break ;;
-        *) echo -e "${RED} Error...${STD}" && sleep 2 ;;
+        esac
+    done
+return
+}
+#  ----------------------------------
+function manageWebserver() {
+while true; do
+    echo -e "\n"$LINE"\n\tManage Docker Nginx web server\n"$LINE 
+    echo -e "  1. Download Nginx Docker Image"
+    echo -e "  2. Status Nginx Docker"
+    echo -e "  5. Restart Nginx Docker"
+    echo -e "  x. Exit\n"$LINE
+    echo -en "\t"; read -rsn1 -p "Enter choice -> " key
+    case $key in
+        1) downloadNginx ;;
+        2) statusContainer nginx ;;
+        5) restartContainer ;;
+        x) break ;;
         esac
     done
 return
@@ -128,24 +76,22 @@ return
 # --------------------------------------------------------------------------
 # Main menu
 initialIssues
+#    echo -en "\t"; read -rsn1 -p "Enter choice -> " key
 while true; do
     clear
     echo -e $LINE"\n\tOptions\n"$LINE 
-    echo -e "  1. Update server & install packages & files"
+    echo -e "  1. Update server & install packages & services"
     echo -e "  2. Update web server config"
-    echo -e "  3. Update web server files & folders structure"
     echo -e "  4. Server maintenance: Backup"
-    echo -e "  5. Manage Nginx web server"
+    echo -e "  7. Manage Docker Nginx web server"
     echo -e "  9. Other services & apps"
     echo -e "  x. Exit\n"$LINE
     echo -en "\t"; read -rsn1 -p "Enter choice -> " key
     case $key in
         1) updateServer ;;
         2) updateWeb ;;
-        3) createStructure ;;
-        4) manageWebserver ;;
+        7) manageWebserver ;;
         x) break ;;
-        *) echo -e "\n\n\t${RED} Error...${STD}" && sleep 2 ;;
     esac
 done
 #
