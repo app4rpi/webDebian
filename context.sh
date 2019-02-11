b1650d1c0b7024e06cd231be15871cb15b3cd7c5
app2linux <46576676+app2linux@users.noreply.github.com>
2019-02-11 06:24:24 +0100

    Update context.sh

--- context.sh
+++ context.sh
@@ -13,6 +13,7 @@ export appFolder="/app/nginx"
 export backupFolder="/app/backup"
 export wwwFolder="/var/www"
 export errorDir="/var/www/error"
+export protectetLocations="/var/www/error /var/www/include"
 export errorStyleLocal=false 		
 #errorStyleLocal=False for Global, [/var/www/error]  | =True for Local, [www<site>/style dir]
 color=('#DC443A' '#98243A' '#11589F' '#715138' '#D69C2F' '#616247' '#898E88' '#2e5090' '#5F4B8B' '#BA0020' '#0E3A53' '#DC443A' '#98243A' '#11589F' '#715138' '#D69C2F' '#616247' '#898E88' '#2e5090' '#5F4B8B')
